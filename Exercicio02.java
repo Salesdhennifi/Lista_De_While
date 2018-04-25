3b03f1385ab5885d99c106be96bc607f191a3b69
Dhennifi Sales <38558685+Salesdhennifi@users.noreply.github.com>
2018-04-25 02:20:28 -0300

    Questão Feita 02
    faltou reparos. Mas a base é essa

---
+++ Exercicio02.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+public class Exercicio02{
+    public static void main(String[] args){
+        //Solicite a idade para o usuário até que ele digite uma idade maior que 128.
+        String nome = "";
+        int idade = 1;
+
+    while(idade <= 129);
+    {
+        JOptionPane.showInputDialog("Digite seu Nome: ");
+        JOptionPane.showInputDialog("Digite sua idade: ");
+        idade = idade + 1;
+    }
+
+
+    }
+}
