3daad93d21a561803ed19a7cecf37aa4b6f08f82
Dhennifi Sales <38558685+Salesdhennifi@users.noreply.github.com>
2018-04-27 10:11:54 -0300

    Questao editada 27/04
    Reformulada

--- Exercicio02.java
+++ Exercicio02.java
@@ -2,16 +2,15 @@ import javax.swing.JOptionPane;
 public class Exercicio02{
     public static void main(String[] args){
         //Solicite a idade para o usuário até que ele digite uma idade maior que 128.
-        String nome = "";
-        int idade = 1;
-
-    while(idade <= 129);
-    {
-        JOptionPane.showInputDialog("Digite seu Nome: ");
-        JOptionPane.showInputDialog("Digite sua idade: ");
-        idade = idade + 1;
-    }
-
+        int quantidadeMaxima = 128;
+        int idade = 0;
+        while(idade < quantidadeMaxima){
+            int recebe = Integer.parseInt(JOptionPane.showInputDialog(null,"Digite o sua idade: "));
+            idade = recebe + 1;
+        }
+        JOptionPane.showMessageDialog(null, "Você esta Saindo do programa...");
 
     }
+
+
 }
