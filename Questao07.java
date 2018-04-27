ee6323ab93c10bb253b4352a4203fafe0c7b0c16
Dhennifi Sales <38558685+Salesdhennifi@users.noreply.github.com>
2018-04-27 16:40:11 -0300

    falta desenvolver
    construir as condiçoes

---
+++ Questao07.java
@@ -0,0 +1,37 @@
+import javax.swing.JOptionPane;
+public class Questao07{
+    public static void main(String[] args){
+        /*Pedir: Nome;Idade;Peso;Sexo;Altura;Quantidade de gols marcados;
+        Quantidade de cartões amarelos recebidos;Quantidade de cartões vermelhos recebidos;
+
+        Mostrar: O jogador com o menor peso;O jogador com a maior altura;O jogador com o maior nome;
+        A quantidade de jogadores do sexo F;A quantidade de jogadores do sexo M
+        O jogador com a menor quantidade de cartões amarelos recebidos;O jogador com o menor nome;
+        O jogador com o maior peso;O jogador com a maior quantidade de cartões vermelhos recebidos;
+        O jogador com a maior quantidade de cartões amarelos recebidos;O jogador com a menor quantidade de cartões vermelhos recebidos.*/
+
+        String nome = "";
+        int idade = 0;
+        int peso = 0;
+        String sexoFemenino = ""; 
+        String sexoMasculino = "";
+        int quantidadeGols = 0;
+        int qtdCartaoAmarelo = 0; 
+        int qtdCartaoVermelho = 0;
+        int cadastro = 0; int auxCad = 1;
+        
+    while(auxCad < cadastro){
+        String recebe = JOptionPane.showInputDialog(null,"Banco de Informação do Time Tabajara Fut!",
+                                                     "\nQuantos Jogadores Deseja Cadastrar? ");
+    auxCad = auxCad + 1;  
+        
+
+        }
+    
+    }
+}
+
+
+
+
+
